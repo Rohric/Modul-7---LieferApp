ea1068b0230036475b46e908e1158858da19d915
Rohric <emilmarsal@icloud.com>
2025-09-18 19:20:28 +0200

    finish style now: refactor JS to one language

--- assets/scripts/orderBox.js
+++ assets/scripts/orderBox.js
@@ -1,4 +1,3 @@
-
 let orderBox = [];
 let total = "";
 
@@ -18,14 +17,13 @@ function renderOrderBox() {
   for (let order = 0; order < orderBox.length; order++) {
     const refOrder = orderBox[order];
 
-    const clickAdd    = `increaseFromOrder(${order})`; // Anzahl ++
-    const clickRemove = `reduceFromOrder(${order})`;   // Anzahl --
-    const clickDelete = `deleteOrder(${order})`;       // sofort löschen
+    const clickAdd = `increaseFromOrder(${order})`; // Anzahl ++
+    const clickRemove = `reduceFromOrder(${order})`; // Anzahl --
+    const clickDelete = `deleteOrder(${order})`; // sofort löschen
 
     // const clickCheckout = `deleteOrder(${order})`;       // checkout, dialog öffnen und alles löschen
 
     orders += templateOrderBox(refOrder, clickAdd, clickDelete, clickRemove);
-
   }
   refOrderFromBox.innerHTML = orders;
 }
@@ -44,7 +42,7 @@ function addToOrder(refCatalog, refCategory, refItem) {
 }
 
 // function zum erhöhen der Anzahl im Warenkorb
-function increaseFromOrder(order){
+function increaseFromOrder(order) {
   const item = orderBox[order]; // daten des Eintrages
   item.Anzahl += 1;
   console.log(orderBox); // FunkionsTest
@@ -62,10 +60,10 @@ function reduceFromOrder(order) {
   orderMath();
 }
 // function zum Löschen der gesamten Anzahl der einzelen Dish im Warenkorb
-function deleteOrder(order){
+function deleteOrder(order) {
   const item = orderBox[order]; // daten des Eintrages
   item.Anzahl = 0;
-  orderBox.splice(order,1); // Aus dem Array slicen
+  orderBox.splice(order, 1); // Aus dem Array slicen
 
   renderOrderBox();
   orderMath();
@@ -73,18 +71,19 @@ function deleteOrder(order){
 }
 
 // checkout, dialog öffnen und alles löschen4
-const dialogRef = document.getElementById('dialog1');
+const dialogRef = document.getElementById("dialogCheckout");
+
 function checkout() {
- 
-    dialogRef.showModal();
-  
+
+  dialogRef.showModal();
+  dialogRef.classList.add('opened')
   // Alle Warenkorb-Items zurücksetzen
   for (let index = 0; index < orderBox.length; index++) {
     const item = orderBox[index];
     item.Anzahl = 0;
   }
   //Warenkorb leeren
- orderBox.splice(0, orderBox.length)
+  orderBox.splice(0, orderBox.length);
 
   renderOrderBox();
   orderMath();
@@ -101,17 +100,21 @@ function orderMath() {
     const price = Number(order.Berry);
     total += amount * price;
   }
-  if (refOrderSum) refOrderSum.textContent =formatBerry(total);
+  if (refOrderSum) refOrderSum.textContent = formatBerry(total);
   return total;
-}
-function closeDialog(){
-  dialogRef.close()
 }
 
 function formatBerry(wert) {
   const nummer = Number(wert);
-  return new Intl.NumberFormat('de-DE', {
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 2
-  }).format(nummer) + ' Berry';
+  return (
+    new Intl.NumberFormat("de-DE", {
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    }).format(nummer) + " Berry"
+  );
+}
+
+function closeDialog() {
+  dialogRef.close();
+  dialogRef.classList.remove("opened");
 }
--- script.js
+++ script.js
@@ -2,15 +2,3 @@ function init() {
   renderAllMainDishes();
 }
 
-
-const dialogRef = document.getElementById("dialogCheckout");
-
-function openDialog() {
-  dialogRef.showModal();
-  dialogRef.classList.add("opened");
-}
-
-function closeDialog() {
-  dialogRef.close();
-  dialogRef.classList.remove("opened");
-}
